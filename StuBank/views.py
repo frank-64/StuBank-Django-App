d834cd2d1420dfc32b704e0219d2225c41894509
RnJhbmsEquals <fcminipad@gmail.com>
2020-12-08 11:07:23 +0000

    Static folder added

--- StuBank/views.py
+++ StuBank/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 # Create your views here.
+
+class Index(View):
+    def index_page(request):
+        return render(request, "index.html")
