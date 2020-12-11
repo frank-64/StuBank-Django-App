3bb49c98bb9e9325554429028bc08f3b1abd4106
RnJhbmsEquals <fcminipad@gmail.com>
2020-12-11 14:33:04 +0000

    - Altered dashboard to display user info - Made transaction to display all transactions

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,9 @@ from dashboard.views import *
 
 urlpatterns = [
     path('', UserDashboardView.as_view(), name='dashboard'),
+    path('transactions/', TransactionView.as_view(), name='transactions'),
+    path('test/', testing, name='test')
+    # path('livechat/', UserDashboardView.as_view(), name='dashboard'),
+    # path('addpayee/', UserDashboardView.as_view(), name='dashboard'),
+    # path('viewpayees/', UserDashboardView.as_view(), name='dashboard'),
 ]
