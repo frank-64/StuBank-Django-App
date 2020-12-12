e735e27855eba78bbd6dda439f5000e12ec676a4
Ed <b9012949@newcastle.ac.uk>
2020-12-12 22:49:25 +0000

    Fixed login redirect error

--- TeamProject/settings.py
+++ TeamProject/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'dashboard_home'
 LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_USER_MODEL = 'accounts.User'
