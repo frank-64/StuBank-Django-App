85488d0d6af3896f8cb77e7adbaf910365bc4da3
Macamood <b9031924@newcastle.ac.uk>
2020-12-15 04:06:28 +0000

    Adding a few things that needed to be added and attempt at models and views (needs work)

--- FakeTransactions/models.py
+++ FakeTransactions/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Food(models.Model):
+    FOODTYPES = (
+        ('Fast Food', 'Fast Food'),
+        ('Chinese', 'Chinese'),
+        ('Groceries', 'Groceries'),
+    )
+
+    FoodType = models.CharField(max_length=20, blank=True, choices=FOODTYPES)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+
+
+class Clothes(models.Model):
+    CLOTHESTYPES = (
+        ('Shirt', 'Shirt'),
+        ('Jeans', 'Jeans'),
+        ('Shorts', 'Shorts'),
+    )
+
+    ClothesType = models.CharField(max_length=20, blank=True, choices=CLOTHESTYPES)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+
+
+class Drink(models.Model):
+    DRINKTYPES = (
+        ('Alcohol', 'Alcohol'),
+        ('Fizzy Drink', 'Fizzy Drink'),
+        ('Water', 'Water'),
+    )
+    DrinkType = models.CharField(max_length=20, blank=True)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+
+
+class Payee(models.Model):
+    PayeeID = models.CharField(max_length=20, blank=True)
+    incoming = models.FloatField(null=False)
+    PayeeDesc = models.CharField(max_length=20, blank=True)
+
+
+class Cheque(models.Model):
+    ChequeId = models.CharField(max_length=20, blank=True)
+    incoming = models.FloatField(null=False)
+    ChequeOwner = models.CharField(max_length=20, blank=True)
--- FakeTransactions/views.py
+++ FakeTransactions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class FakeTransactions(View):
+
+    def AddNewTransaction(request):
+        return render(request, 'FakeTransactions/transactionPage.html')
