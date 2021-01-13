f59eb77a76c26fd84158977efbbd077eaa98c4a7
Ed <b9012949@newcastle.ac.uk>
2021-01-13 15:54:09 +0000

    Added missing link to css file in base templates

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -16,27 +16,29 @@ from .models import *
 from .views import *
 from dashboard import forms
 
-def setUpUser():
-    user = User.objects.create(username='test_customer', email='customer@test.com', first_name='Bobby',
+
+def setUpUser(username):
+    user = User.objects.create(username=username, email='customer@test.com', first_name='Bobby',
                                last_name='Hummer', is_customer=1)
     user.set_password('password')
     user.save()
     return user
+
 
 class CheckPayeeTestCase(TestCase):
     # Create the customer and helper objects in the database for testing
     def setUp(self):
         self.request_factory = RequestFactory()
         self.user = User.objects.create(username='test_customer', email='customer@test.com', first_name='Bobby',
-                                            last_name='Hummer', is_customer=1)
+                                        last_name='Hummer', is_customer=1)
         self.user.set_password('password')
 
         self.user_payee = User.objects.create(username='test_payee', email='helper@test.com', first_name='Pegasus',
-                                          last_name='Smores', is_customer=0, is_helper=1)
+                                              last_name='Smores', is_customer=0, is_helper=1)
         self.user_payee.set_password('qwerty')
 
         self.payee = Customer.objects.create(user=self.user_payee, account_num=2222222, sort_code='42-04-20',
-                                                 balance=100.00, available_balance=100.00)
+                                             balance=100.00, available_balance=100.00)
         self.user.save()
         self.payee.save()
 
@@ -46,17 +48,17 @@ class CheckPayeeTestCase(TestCase):
 
     def test_check_payee_exists(self):
         payee_object = Customer.objects.filter(sort_code=self.payee.sort_code,
-                                                 account_num=self.payee.account_num,
-                                                 user__first_name=self.payee.user.first_name,
-                                                 user__last_name=self.payee.user.last_name)
+                                               account_num=self.payee.account_num,
+                                               user__first_name=self.payee.user.first_name,
+                                               user__last_name=self.payee.user.last_name)
 
         self.assertEqual(True, payee_object.exists())
 
     def test_payee_same_as_user(self):
         payee_object = Customer.objects.filter(sort_code=self.payee.sort_code,
-                                                 account_num=self.payee.account_num,
-                                                 user__first_name=self.payee.user.first_name,
-                                                 user__last_name=self.payee.user.last_name)
+                                               account_num=self.payee.account_num,
+                                               user__first_name=self.payee.user.first_name,
+                                               user__last_name=self.payee.user.last_name)
 
         self.assertEqual(True, payee_object[0].user == self.payee.user)
 
@@ -65,15 +67,15 @@ class AddPayeeTestCase(TestCase):
     def setUp(self):
         self.request_factory = RequestFactory()
         self.user = User.objects.create(username='test_customer', email='customer@test.com', first_name='Bobby',
-                                            last_name='Hummer', is_customer=1)
+                                        last_name='Hummer', is_customer=1)
         self.user.set_password('password')
 
         self.user_payee = User.objects.create(username='test_payee', email='helper@test.com', first_name='Pegasus',
-                                          last_name='Smores', is_customer=0, is_helper=1)
+                                              last_name='Smores', is_customer=0, is_helper=1)
         self.user_payee.set_password('qwerty')
 
         self.payee = Customer.objects.create(user=self.user_payee, account_num=2222222, sort_code='42-04-20',
-                                                 balance=100.00, available_balance=100.00)
+                                             balance=100.00, available_balance=100.00)
 
         self.user.save()
         self.payee.save()
@@ -83,13 +85,6 @@ class AddPayeeTestCase(TestCase):
         print(Customer.objects.all())
         request = self.request_factory.get(reverse('addpayee'))
         request.user = self.user
-
-def setUpUser(username):
-    user = User.objects.create(username=username, email='customer@test.com', first_name='Bobby',
-                               last_name='Hummer', is_customer=1)
-    user.set_password('password')
-    user.save()
-    return user
 
 
 class MoneyPotCreateViewTestCase(TestCase):
@@ -214,7 +209,7 @@ class MoneyPotUpdateViewTestCase(TestCase):
     # Test pot details are not updated if invalid data is input in form
     def test_post_fail(self):
         response = self.client.post(reverse('update_money_pot', kwargs={'pk': self.pk}), data={'name': 'updated_name',
-                                                                                        'target_balance': '#########'})
+                                                                                               'target_balance': '#########'})
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.failIf(response.context['form'].is_valid())
@@ -328,8 +323,10 @@ class HelpPageViewTestCase(TestCase):
         response = self.client.get(reverse('help'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+
 class LiveChatTestCase(TestCase):
     pass
+
 
 '''class LiveChatTestCase(TestCase):
 
