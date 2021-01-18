e9b0e8cf501fb37ab470824565cef2528f975ad1
Macamood <b9031924@newcastle.ac.uk>
2021-01-18 19:24:23 +0000

    Base template working and most script working

--- static/javascript/StuShopButtons.js
+++ static/javascript/StuShopButtons.js
@@ -8,7 +8,7 @@ function ready() {
     var stuAdd = document.getElementsByClassName('StuAdd')
     for (let i = 0; i < stuAdd.length; i++) {
         let button = stuAdd[i];
-        button.addEventListener('click',addToQuantity) //function of the button
+        button.addEventListener('click',addButton) //function of the button
     }
     var stuRemove = document.getElementsByClassName('StuDelete')
     for (let i = 0; i < stuRemove.length; i++) {
@@ -17,11 +17,45 @@ function ready() {
     }
 }
 
-function addToQuantity(){
+function addButton(press){
+    let pressed = press.target;
+    let StuItem = pressed.parentElement.parentElement;
+    var StuTitleAdd = StuItem.getElementsByClassName('StuTitle')[0].innerText;
+    var StuPriceAdd = StuItem.getElementsByClassName('StuPrice')[0].innerText;
+    var StuStuff = {price:StuPriceAdd, title:StuTitleAdd}
+    addToCart(StuStuff)
+    console.log(StuStuff)
+}
+
+
+function addToCart(StuStuff){
+    var row = document.createElement('div')
+    row.classList.add('StuCart')
+    let stuff = StuStuff
+    var StuTable = document.getElementsByClassName('StuTable')[0]
+    var StuNames = StuTable.getElementsByClassName('StuCartTitle')
+    for (let i = 0; i < StuNames.length; i++) {
+        if (StuNames[i].innerText === stuff.title) {
+            alert('If you want to add more, change the quantity :D')
+            return
+        }
+    }
+    row.innerHTML = `<div class="cart-item cart-column">
+            <span class="StuCartTitle">` + stuff.title + `</span>
+        </div>
+        <span class="StuCartPrice">`+ stuff.price + `</span>
+        <div class="StuInputs">
+            <input class="StuQuantity" type="number" value="1">
+            <button class="StuDelete" type="button">Remove</button>
+        </div>`
+    StuTable.append(row)
+    row.getElementsByClassName('StuDelete')[0].addEventListener('click', remove)
+   // row.getElementsByClassName('StuQuantity')[0].addEventListener('change', (add function here) )
 
 }
-function remove(){
-
+function remove(press){
+    let pressed = press.target;
+    pressed.parentElement.parentElement.remove()
 }
 
 
